a27a3792ac00b34dfa3c726f5f0fd45757af0fa1
Andrew Cole <aocole@gmail.com>
2013-12-15 12:17:03 -0800

    Remove capistrano stuff

--- config/deploy/development.rb
+++
@@ -1,7 +0,0 @@
-set :user, "andrewcole"
-set :application, "list.skillshot.ndrew.org"
-set :deploy_to, "/home/#{user}/#{application}"
-role :web, application                          # Your HTTP server, Apache/etc
-role :app, application                          # This may be the same as your `Web` server
-role :db,  application, :primary => true # This is where Rails migrations will run
-
--- config/deploy/production.rb
+++
@@ -1,6 +0,0 @@
-set :user, "aocole"
-set :application, "list.skill-shot.com"
-set :deploy_to, "/home/#{user}/#{application}"
-role :web, application                          # Your HTTP server, Apache/etc
-role :app, application                          # This may be the same as your `Web` server
-role :db,  application, :primary => true # This is where Rails migrations will run
