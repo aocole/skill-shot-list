44c001a95cb01ef703b548cf4674f12d4a765740
Andrew Cole <aocole@gmail.com>
2014-10-08 11:33:47 -0700

    More history

--- reconstruct.rb
+++ reconstruct.rb
@@ -10,12 +10,108 @@ m = MachineChange.
 
 require 'yaml'
 require 'colored'
-locations = Location.unscoped
-locations = Hash[locations.collect{|loc| [loc.name, loc]}]
-locations['12th Ave Laundry'] = Location.find_by_cached_slug 'lather-daddy'
 
 localities = Locality.unscoped
 localities = Hash[localities.collect{|loc| [loc.name, loc]}]
+
+locations = Location.unscoped
+locations = Hash[locations.collect{|loc| [loc.name, loc]}]
+locations['12th Ave Laundry'] = Location.find_by_cached_slug 'lather-daddy'
+locations['ADD Motor Works'] = Location.find_by_cached_slug 'add-a-ball-amusements'
+old_locations =<<END
+---
+Downtown:
+  - Thistle
+  - Fenix
+Columbia City:
+  - AMF Imperial Lanes
+U District/North Side:
+  - Galway Arms
+  - Piccolo's Pizza
+  - Rat and Raven
+  - Cafe Racer
+  - South Campus Center
+  - Pink Gorilla
+  - Shanty Tavern
+  - Resevoir
+Greenwood/Green Lake:
+  - Little Red Hen
+  - Sundown Saloon
+  - Sweet Lou's
+SODO:
+  - Goldies
+  - Club Motor
+Georgetown:
+  - Calamity Jane's
+  - Tiger Lounge
+  - Auto Quest
+  - Uncle Mo's
+Pioneer Square/International District:
+  - Cowgirls Inc
+  - Fuel
+Belltown/Denny Regrade:
+  - 5 Point Laundry
+  - Lava Lounge
+  - Nite Lite
+Capitol Hill:
+  - Elite
+  - Auto Battery
+  - Chieftain
+  - Wild Rose
+  - Neighbours
+  - Eagle
+  - King Cobra
+  - Elite
+Fremont/Wallingford:
+  - Fremont Dock
+  - High Dive
+  - Buckaroo Tavern
+  - Dubliner
+  - Sock Monster
+  - Pete's Fremont Fire Pit
+Ballard:
+  - Snoose Junction
+  - Molly Maguires
+  - Zesto's
+  - Goofy's
+Eastlake:
+  - El Corazon
+  - Cafe Venus
+Queen Anne:
+  - Jabu's
+  - Jillian's
+  - Funhouse
+  - Hula Hula
+  - Floyd's Place
+  - Fun Forest Arcade
+  - Ozzie's
+  - Spectator
+  - Bandit's
+West Seattle:
+  - Alki Tavern
+  - Rocksports
+  - Shipwreck Tavern
+  - Admiral Pub
+  - Feedback Lounge
+  - Roxbury Lanes
+  - Tug Tavern
+White Center:
+  - Barrel Tavern
+  - Brewsky's
+  - Papa's
+  - Marv's Boiler
+  - Wall Bar
+South Lake Union:
+  - Lunch Box Laboratory
+END
+old_locations = YAML.load(old_locations)
+old_locations.each do |locality_name, location_names|
+  locality = localities[locality_name]
+  raise "Couldn't find #{locality_name}" if locality.nil?
+  location_names.each do |location_name|
+    locations[location_name] = Location.new(locality: locality, name: location_name)
+  end
+end
 
 titles = Title.unscoped
 titles = Hash[titles.collect{|loc| [loc.name, loc]}]
@@ -57,7 +153,24 @@ Hash[
   'NGG', 'No Good Gofers',
   'IJ', 'Indiana Jones (Williams)',
   'IJ(S)', 'Indiana Jones (Stern)',
-  'GNR', "Guns N' Roses"
+  'GNR', "Guns N' Roses",
+  'IM', "Iron Man",
+  'TRON', "TRON Legacy",
+  'Shadow', "The Shadow",
+  'RS', "Red & Ted's Road Show",
+  'DESW', "Star Wars (Data East)",
+  'Jack-Bot', "JackBot",
+  'Pinbot', "PINBOT",
+  'Four Million BC', "Four Million B.C.",
+  'Hulk', 'The Incredible Hulk',
+  'Transformers', 'Transformers (Pro)',
+  'ST', "Star Trek (Data East)",
+  'BBH', 'Big Buck Hunter Pro',
+  'EBD', 'Eight Ball Deluxe (1980 Bally)',
+  'RBION', "Ripley's Believe It or Not!",
+  'EATPM', "Elvira and the Party Monsters",
+  'WPT', "World Poker Tour",
+  'JM', "Johnny Mnemonic"
 ].each do |abbrev, name|
   titles[abbrev] = Title.where(name: name).first
   raise "Couldn't find #{name}" if titles[abbrev].nil?
