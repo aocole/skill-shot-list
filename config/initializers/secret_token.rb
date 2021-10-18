1a25a317ec05587544aea34d327527c3161216ec
Andrew Cole <aocole@gmail.com>
2021-10-17 22:38:18 -0700

    update generated configs for rails 4

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,9 +1,18 @@
 # Be sure to restart your server when you modify this file.
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
 if %w(development test).include? Rails.env
-  Skillshot::Application.config.secret_token = 'c74363ccf2373ccfdd2acdea4161fba381a593735df64242cd8468edb0987d42f5a6070c33eb5c770a5e8004e097e61e49088652bec61'
+  Skillshot::Application.config.secret_key_base = '0c6aefae71130657b95fb08e33a88ee080099721e9288dc4936a092c61ee57ffcaecf5ddc0f51849ec00bc7e881420cd6f2dbc3a83d6bbbf3117aa84479b1c9d'
 else
   if ENV['SECRET_TOKEN'].present?
-    Skillshot::Application.config.secret_token = ENV['SECRET_TOKEN']
+    Skillshot::Application.config.secret_key_base = ENV['SECRET_TOKEN']
 
   # Do not raise an error if secret token is not available during assets precompilation
   elsif ENV['RAILS_GROUPS'] != 'assets'
