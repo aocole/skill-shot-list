2528db0f7950b54116068451cfb6e3c7d07cf1a0
Andrew Cole <aocole@gmail.com>
2022-11-20 18:40:34 -0800

    fix cache expiry

--- app/models/machine_sweeper.rb
+++ app/models/machine_sweeper.rb
@@ -21,10 +21,10 @@ class MachineSweeper < ActionController::Caching::Sweeper
   private
   def expire_cache_for(machine)
     # Expire the index page now that we added a new machine
-    expire_action(controller: 'areas', action: 'show', id: machine.location.area)
-    expire_action(controller: 'locations', action: 'show', id: machine.location)
-    expire_action(controller: 'titles', action: 'active')
-    expire_action(controller: 'locations', action: 'show', id: machine.location, format: 'json')
+    controller.expire_action(controller: 'areas', action: 'show', id: machine.location.area)
+    controller.expire_action(controller: 'locations', action: 'show', id: machine.location)
+    controller.expire_action(controller: 'titles', action: 'active')
+    controller.expire_action(controller: 'locations', action: 'show', id: machine.location, format: 'json')
     expire_fragment('trivia')
   end
 
